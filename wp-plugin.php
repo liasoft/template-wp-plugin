50189c59c1de994782e232dfba19544508bd4c5f
Sven Scharmentke <24257042+svnscha@users.noreply.github.com>
2025-10-25 14:54:12 +0200

    feat: migrate to GPL-3.0-or-later license and update requirements (#3)

--- wp-plugin.php
+++ wp-plugin.php
@@ -6,9 +6,9 @@
  * Version: 1.0.3
  * Author: Liasoft GmbH
  * Author URI: https://github.com/liasoft/template-wp-plugin
- * License: Apache 2.0
- * License URI: https://www.apache.org/licenses/LICENSE-2.0
- * Requires at least: 6.0
+ * License: GPL-3.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Requires at least: 6.5
  * Tested up to: 6.8
  * Requires PHP: 8.3
  *
